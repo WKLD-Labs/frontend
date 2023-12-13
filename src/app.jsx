a64d2ab0d588f959a0b424a602d79823b74bd216
adamrafiffaqih <90492899+AdamRFaqih@users.noreply.github.com>
2023-12-13 09:11:51 +0700

    integrate backend
    use session for login

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation, Navigate } from "react-router-dom";
 import './app.css'
 
 import Mainlayout from "./layouts/Main";
@@ -17,6 +17,10 @@ function ScrollToTop() {
 }
 
 export function App() {
+  const isLogin = () => {
+    const token = sessionStorage.getItem('token');
+    return !!token;
+  };
   return (
     <BrowserRouter>
       <ScrollToTop/>
@@ -24,6 +28,12 @@ export function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Mainlayout/>}>
           <Route index element={<Homepage />} />
+          <Route path="/agenda" element={isLogin() ? <Agenda /> : <Navigate to="/login" />} />
+          <Route path="/inventory" element={isLogin() ? <Inventory /> : <Navigate to="/login" />} />
+          <Route path="/roomschedule" element={isLogin() ? <JadwalRuangan /> : <Navigate to="/login" />} />
+          <Route path="/schedule" element={isLogin() ? <Jadwal /> : <Navigate to="/login" />} />
+          <Route path="/memberdata" element={isLogin() ? <Anggota /> : <Navigate to="/login" />} />
+          <Route path="/documents" element={isLogin() ? <Document /> : <Navigate to="/login" />} />
           <Route path="*" element={<h1 class="w-full text-center text-3xl my-auto">404</h1>} />
         </Route>
       </Routes>
