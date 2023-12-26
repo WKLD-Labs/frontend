b350f62fa7cd64c465b97b0147f2fe5e41831b47
M Isa Al Anshori <isabungsu@gmail.com>
2023-12-26 21:28:53 +0700

    WEBPRO-21 Jadwal Ruangan: Initial Integration

--- src/pages/JadwalRuangan/JadwalRuangan.jsx
+++ src/pages/JadwalRuangan/JadwalRuangan.jsx
@@ -1,14 +1,71 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import ScheduleCalendar from "./components/ScheduleCalendar";
-import { MdOutlineDelete, MdMoreVert } from "react-icons/md";
+import { MdOutlineDelete, MdMoreVert, MdEdit } from "react-icons/md";
 import { PopUpDialog, PopUpActions, PopUpContents, PopUpHeader } from "../../components/PopUpDialog";
 import { Table, TableHeader, TableBody, TableRow, TableCol } from '../../components/Table';
 
-function NewActivityDialog({showDialog, setShowDialog}) {
-    const [date, setDate] = useState(new Date());
+function PopUpPrompt({setCaller}) {
+    const [header, setHeader] = useState("");
+    const [contents, setContents] = useState("");
+    const [visible, setVisible] = useState(false);
+    const [promise, setPromise] = useState(null);
+
+    const caller = (header, contents) => {
+        setHeader(header);
+        setContents(contents);
+        setVisible(true);
+        return new Promise((resolve, reject) => {
+            setPromise({resolve, reject});
+        })
+    }
+
+    useEffect(() => setCaller(caller), []);
+
+    return (
+        <PopUpDialog open={visible} onChange={() => promise.resolve(false)}>
+            <PopUpHeader text={header}/>
+            <PopUpContents>
+                {contents}
+            </PopUpContents>
+            <PopUpActions>
+                <button className="py-1 px-4 rounded-full bg-aseorange text-white" onClick={() => {promise.resolve(true); setVisible(false)}}>Yes</button>
+                <button className="py-1 px-4 rounded-full bg-aseorange text-white" onClick={() => {promise.resolve(false); setVisible(false)}}>No</button>
+            </PopUpActions>
+        </PopUpDialog>
+    )
+}
+
+function NewActivityDialog({showDialog, setShowDialog, editData, onSubmit, markedDates}) {
+    const [formData, setFormData] = useState({name: "", start_date: "", end_date: ""});
+    console.log(formData);
+    if (editData) {
+        markedDates = markedDates.filter((e) => e.start.getTime() != editData.start.getTime() && e.end.getTime() != editData.end.getTime());
+    }
+    markedDates = [...markedDates, 
+        {
+            start: new Date(formData.start_date),
+            end: new Date(formData.end_date),
+            state: "selected"
+        }
+    ];
+    useEffect(() => {
+        if (editData) {
+            setFormData({id: editData.id, name: editData.name, start_date: editData.start.toISOString().slice(0, -1), end_date: editData.end.toISOString().slice(0, -1)});
+            
+        } else {
+            setFormData({id: null, name: "", start_date: "", end_date: ""});
+        }
+    }, [editData])
+    function handleChange(e) {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    }
+    function closeDialog(e) {
+        setShowDialog(e);
+        setFormData({id: null, name: "", start: "", end: ""})
+    }
     return (
         <PopUpDialog open={showDialog} onChange={setShowDialog}>
-            <PopUpHeader text="New Activity"/>
+            <PopUpHeader text={editData ? "Edit Schedule" : "New Schedule"}/>
             <PopUpContents>
                 <div className="flex flex-col md:flex-row gap-4 items-center overflow-x-hidden">
                     <form className="flex flex-col gap-2 flex-1">
@@ -16,77 +73,97 @@ function NewActivityDialog({showDialog, setShowDialog}) {
                             <tbody>
                                 <tr>
                                     <td className="w-32">Meeting Name</td>
-                                    <td><input className="aseinput w-full" type="text" /></td>
+                                    <td><input className="aseinput w-full" type="text" name="name" value={formData.name} onChange={handleChange} /></td>
                                 </tr>
                                 <tr>
                                     <td className="w-32">Start Time</td>
-                                    <td><input className="aseinput" type="date" /></td>
+                                    <td><input className="aseinput" type="datetime-local" name="start_date" value={formData.start_date} onChange={handleChange}/></td>
                                 </tr>
-                                <tr>
-                                    <td className="w-32"></td>
-                                    <td><input className="aseinput" type="time" /></td>
-                                </tr>
-                                <tr>
+                                <tr>    
                                     <td className="w-32">End Time</td>
-                                    <td><input className="aseinput" type="date" /></td>
-                                </tr>
-                                <tr>
-                                    <td className="w-32"></td>
-                                    <td><input className="aseinput" type="time" /></td>
+                                    <td><input className="aseinput" type="datetime-local" name="end_date" value={formData.end_date}  onChange={handleChange}/></td>
                                 </tr>
                             </tbody>
                         </table>
                     </form>
                     <div>
-                        <ScheduleCalendar currentDate={date} onDateUpdate={setDate} markedDates={[
-                                {
-                                    start: new Date("11 Oct 2023, 15:00"),
-                                    end: new Date("14 Oct 2023, 12:00"),
-                                    state: "selected"
-                                }
-                            ]
-                        } />
+                        <ScheduleCalendar readOnly={true} markedDates={markedDates} />
                     </div>
                 </div>
                 
             </PopUpContents>
             <PopUpActions>
-                <button className="py-1 px-4 rounded-full bg-aseorange text-white" onClick={() => {alert("Pura Puranya di save");setShowDialog(false)}}>Save</button>
-                <button className="py-1 px-4 rounded-full bg-aseorange text-white" onClick={()=>setShowDialog(false)}>Close</button>
+                <button className="py-1 px-4 rounded-full bg-aseorange text-white" onClick={() => {onSubmit(formData); closeDialog(false); }}>{editData ? "Update" : "Save"}</button>
+                <button className="py-1 px-4 rounded-full bg-aseorange text-white" onClick={()=> {closeDialog(false)}}>Close</button>
             </PopUpActions>
         </PopUpDialog>
     )
 }
 
 export default function JadwalRuangan() {
-    const [date, setDate] = useState(new Date());
+    const bearertoken = sessionStorage.getItem("token");
+    const apiurl = "http://localhost:5500/api/roomschedule";
+    const [calendarDate, setCalendarDate] = useState(new Date());
     const [scheduleData, setScheduleData] = useState([]);
     const [showDialog, setShowDialog] = useState(false);
+    const [selectedData, setSelectedData] = useState(null);
+    const aPrompt = useRef(null);
+    console.log(scheduleData)
+    async function handleSubmit(data) {
+        const headers = {
+            'Authorization': 'Bearer ' + bearertoken,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        let response;
+        if (data.id) {  
+            response = await fetch(apiurl + "/" + data.id, {method: 'PUT', headers, body: new URLSearchParams(data)});
+        } else {
+            response = await fetch(apiurl, {method: 'POST', headers, body: new URLSearchParams(data)});
+        }
+        updateScheduleData();
+        return response;
+    }
+
+    async function handleDelete(id) {
+        const headers = {
+            'Authorization': 'Bearer ' + bearertoken
+        }
+        if (await aPrompt.current("Delete Schedule", "Are you sure you want to delete this schedule?") == false) return;
+        const response = await fetch(apiurl + "/" + id, {method: 'DELETE', headers});
+        updateScheduleData()
+    }
+
+    async function getSchedule(month, year) {
+        // GET $apiurl?month=$month&year=$year with bearer token, return json
+        let response;
+        const headers = {
+            'Authorization': 'Bearer ' + bearertoken
+        }
+        if (month && year) {
+            response = await fetch(apiurl + "?month=" + month + "&year=" + year, {headers});
+        } else {
+            response = await fetch(apiurl, {headers});
+        }
+        let data = await response.json();
+        console.log("got:", data);
+        data = data.map((e) => {
+            return {
+                id: e.id,
+                name: e.name,
+                start: new Date(e.start_date),
+                end: new Date(e.end_date)
+            }
+        })
+        console.log("loaded:", data);
+        return data;
+    }
+    async function updateScheduleData() {
+        const data = await getSchedule(calendarDate.getMonth() + 1, calendarDate.getFullYear());
+        setScheduleData(data);
+    }
     useEffect(() => {
-        setScheduleData([
-            {
-                name: "Nama Kegiatan",
-                start: new Date("11 Oct 2023, 15:00"),
-                end: new Date("12 Oct 2023, 12:00")
-            },
-            {
-                name: "Nama Kegiatan",
-                start: new Date("23 Oct 2023, 12:00"),
-                end: new Date("24 Oct 2023, 12:00")
-            },
-            {
-                name: "Nama Kegiatan 1 2 3 4 5",
-                start: new Date("23 June 2023, 12:00"),
-                end: new Date("24 June 2023, 12:00")
-            },
-            {
-                name: "Nama Kegiatan",
-                start: new Date("23 June 2023, 12:00"),
-                end: new Date("24 June 2023, 12:00")
-            },
-            
-        ])
-    }, [])
+        updateScheduleData();
+    }, [calendarDate])
     const formatDate = (date) => {
         const options = {
             day: 'numeric',
@@ -97,7 +174,7 @@ export default function JadwalRuangan() {
         };
         return date.toLocaleDateString('id-ID', options);
     }
-    console.log(date)
+    // console.log(date)
     return (
       <div className="p-16 flex flex-col gap-4 flex-1">
         <h1 className="text-3xl font-semibold max-sm:text-center">Schedule</h1>
@@ -118,12 +195,12 @@ export default function JadwalRuangan() {
                             <TableCol>{formatDate(data.end)}</TableCol>
                             <TableCol>
                                 <span className="ml-auto w-fit flex flex-row gap-2">
-                                    <button onClick={()=>alert("DELETE " + data.name )}>
+                                    <button onClick={()=>handleDelete(data.id)}>
                                         <MdOutlineDelete size={24} />
                                     </button>
-                                    <button onClick={()=>alert("MORE " + data.name )}>
-                                        <MdMoreVert size={24} />
-                                    </button>
+                                    <button onClick={()=>{setSelectedData(data); setShowDialog(true)}}>
+                                        <MdEdit size={24} />
+                                    </button>   
                                     
                                 </span>
                             </TableCol>
@@ -131,27 +208,30 @@ export default function JadwalRuangan() {
                     ))}
                 </TableBody>
             </Table>
+            {
+                scheduleData.length == 0 && <div className="text-center">No Schedule</div>
+            }
             </div>
             <div className="flex flex-col gap-4 ">
-                <ScheduleCalendar currentDate={date} onDateUpdate={setDate} markedDates={[
-                        {
-                            start: new Date("11 Oct 2023, 15:00"),
-                            end: new Date("14 Oct 2023, 12:00"),
-                            state: "occupied"
-                        },
-                        {
-                            start: new Date("23 Nov 2023, 12:00"),
-                            end: new Date("24 Nov 2023, 12:00"),
-                            state: "occupied"
-                        }
-                    ]
-                } />
-                <button onClick={()=>setShowDialog(true)} className="self-center lg:self-end rounded-xl w-48 h-8 bg-white text-black border border-asegrey">New Schedule</button>
-                <NewActivityDialog showDialog={showDialog} setShowDialog={setShowDialog}/>
+                <ScheduleCalendar readOnly={true} calendarDate={calendarDate} onCalendarDateUpdate={setCalendarDate} markedDates={scheduleData.map(e=>{
+                    return {
+                        start: e.start,
+                        end: e.end,
+                        state: "occupied"
+                    }
+                })} />
+                <button onClick={()=>{setSelectedData(null); setShowDialog(true)}} className="self-center lg:self-end rounded-xl w-48 h-8 bg-white text-black border border-asegrey">New Schedule</button>
+                <NewActivityDialog showDialog={showDialog} setShowDialog={setShowDialog} editData={selectedData} onSubmit={handleSubmit} markedDates={scheduleData.map(e=>{
+                    return {
+                        start: e.start,
+                        end: e.end,
+                        state: "occupied"
+                    }
+                })}/>
             </div>
           
         </div>
-        
+        <PopUpPrompt setCaller={v=>{aPrompt.current = v}} />
       </div>
     );
 }
--- src/pages/JadwalRuangan/components/ScheduleCalendar.jsx
+++ src/pages/JadwalRuangan/components/ScheduleCalendar.jsx
@@ -1,34 +1,38 @@
 import { useState } from "react";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
-export default function ScheduleCalendar({currentDate, onDateUpdate, readOnly, markedDates}) {
-  const [date, setDate] = useState(currentDate || new Date());
+export default function ScheduleCalendar({currentDate, onDateUpdate, calendarDate, onCalendarDateUpdate, readOnly, markedDates}) {
   currentDate = currentDate || new Date();
+  calendarDate = calendarDate || currentDate;
   function setCurrentDate(newDate) {
     if (readOnly !== undefined && readOnly == true) return;
     onDateUpdate(newDate);
   }
 
+  function setCalendarDate(newDate) {
+    onCalendarDateUpdate(newDate);
+  }
+
   function updateDateSelection(newDate) {
-    setCurrentDate(new Date(date.getFullYear(), date.getMonth(), newDate));
+    setCurrentDate(new Date(calendarDate.getFullYear(), calendarDate.getMonth(), newDate));
   }
 
   const daysInMonth = (month) => {
-    const year = date.getFullYear();
+    const year = calendarDate.getFullYear();
     return new Date(year, month, 0).getDate();
   };
 
   const startDay = () => {
-    const year = date.getFullYear();
-    const month = date.getMonth();
+    const year = calendarDate.getFullYear();
+    const month = calendarDate.getMonth();
     return new Date(year, month, 1).getDay();
   };
 
   const renderCalendar = () => {
-    const totalDays = daysInMonth(date.getMonth() + 1);
-    const prevTotalDays = daysInMonth(date.getMonth());
+    const totalDays = daysInMonth(calendarDate.getMonth() + 1);
+    const prevTotalDays = daysInMonth(calendarDate.getMonth());
     const firstDay = startDay();
-    const viewCurrentMonth = date.getFullYear() === currentDate.getFullYear() && date.getMonth() === currentDate.getMonth();
+    const viewCurrentMonth = calendarDate.getFullYear() === currentDate.getFullYear() && calendarDate.getMonth() === currentDate.getMonth();
     const days = [];
 
     for (let i = 1; i <= totalDays + firstDay; i++) {
@@ -36,40 +40,31 @@ export default function ScheduleCalendar({currentDate, onDateUpdate, readOnly, m
         days.push(<div key={i} className="text-gray-400 flex items-center justify-center aspect-square">{prevTotalDays - (firstDay - i)}</div>);
       } else {
         let dateStyle = ""
-        const thisDate = new Date(date.getFullYear(), date.getMonth(), i - firstDay).getTime();
+        const thisDate = new Date(calendarDate.getFullYear(), calendarDate.getMonth(), i - firstDay).getTime();
 
         if (viewCurrentMonth && i - firstDay === currentDate.getDate()) {
           dateStyle = "bg-green-400 font-medium rounded-full"
-        } else if (markedDates) {
+        }
+        if (markedDates) {
           markedDates.forEach((occupiedDate) => {
-            const bgColor = {
+            occupiedDate = {
+              ...occupiedDate,
+              start: new Date(occupiedDate.start),
+              end: new Date(occupiedDate.end)
+            }
+            let bgColor = {
               "occupied": "bg-red-400",
               "selected": "bg-blue-400"
             }[occupiedDate.state]
+            if (viewCurrentMonth && i - firstDay === currentDate.getDate()) {
+              bgColor = "bg-green-400"
+            }
             if (occupiedDate.start.setHours(0, 0, 0, 0) == thisDate) {
-              if (viewCurrentMonth && i - firstDay == currentDate.getDate()-1 ) {
-                dateStyle = `rounded-full ${bgColor}`
-              } else {
-                dateStyle = `rounded-l-full ${bgColor}`
-              }
-              
+              dateStyle = `rounded-l-full ${bgColor}`
             } else if (occupiedDate.end.setHours(0, 0, 0, 0) == thisDate) {
-              if (viewCurrentMonth &&  viewCurrentMonth && i - firstDay - 1 == currentDate.getDate()) {
-                dateStyle = `rounded-full ${bgColor}`
-              } else {
-                dateStyle = `rounded-r-full ${bgColor}`
-              }
-              
+              dateStyle = `rounded-r-full ${bgColor}`
             } else if (occupiedDate.start.setHours(0, 0, 0, 0) < thisDate && occupiedDate.end.setHours(0, 0, 0, 0) > thisDate) {
-              if (viewCurrentMonth && i - firstDay == currentDate.getDate()-1) {
-                dateStyle = `rounded-r-full ${bgColor}`
-              } else if (viewCurrentMonth && i - firstDay - 1 == currentDate.getDate()) {
-                dateStyle = `rounded-l-full ${bgColor}`
-              } else {
-                dateStyle = `${bgColor}`
-              }
-              
-              
+              dateStyle = `${bgColor}`
             }
           })
         }
@@ -94,11 +89,11 @@ export default function ScheduleCalendar({currentDate, onDateUpdate, readOnly, m
   return (
     <div className="w-80 h-fit text-center p-4 mx-2 bg-neutral-100 rounded-sm font-light">
       <div className="flex items-center gap-4 mb-4">
-        <button onClick={() => setDate(new Date(date.getFullYear(), date.getMonth() - 1))}><MdChevronLeft size="32px"/></button>
+        <button onClick={() => setCalendarDate(new Date(calendarDate.getFullYear(), calendarDate.getMonth() - 1))}><MdChevronLeft size="32px"/></button>
         <div>
-          {date.toLocaleString('default', { month: 'long', year: 'numeric' })}
+          {calendarDate.toLocaleString('default', { month: 'long', year: 'numeric' })}
         </div>
-        <button onClick={() => setDate(new Date(date.getFullYear(), date.getMonth() + 1))}><MdChevronRight size="32px"/></button>
+        <button onClick={() => setCalendarDate(new Date(calendarDate.getFullYear(), calendarDate.getMonth() + 1))}><MdChevronRight size="32px"/></button>
       </div>
       <div className="grid grid-cols-7 gap-1 text-center mb-8">
         <div>Su</div>
