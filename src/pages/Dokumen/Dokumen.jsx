d4f821669e3c0d34e133e6554263696ab0fc42b9
MaximusBayu <maximusbayu9b16@gmail.com>
2024-01-03 07:08:55 +0700

    Bisa Create Dokumen

--- src/pages/Dokumen/Dokumen.jsx
+++ src/pages/Dokumen/Dokumen.jsx
@@ -4,7 +4,22 @@ import { MdPersonOutline, MdMenuBook, MdOutlineDelete, MdMoreVert } from "react-
 import { PopUpDialog, PopUpHeader, PopUpContents, PopUpActions } from "../../components/PopUpDialog";
 import { useState, useEffect } from "react";
 
-function NewMeeting({showDialog, setShowDialog}) {
+function NewDocument({showDialog, setShowDialog, onSubmit}) {
+    const [doc, setDoc] = useState({title: '', writer: '', description: ''});
+    
+    function handleTitle(e) {
+        setDoc({...doc, title: e.target.value});
+    }
+    function handleWriter(e) {
+        setDoc({...doc, writer: e.target.value});
+    }
+    function handleDesc(e) {
+        setDoc({...doc, description: e.target.value});
+    }
+    function handleSave() {
+        onSubmit(doc);
+    }
+
     return(
         <div className=''>
             <PopUpDialog open={showDialog} onChange={setShowDialog}>
@@ -17,7 +32,7 @@ function NewMeeting({showDialog, setShowDialog}) {
                                     <td style="width:20%"><h1 className='inline-block'>Title</h1></td>
                                     <td>
                                         <div className='flex flex-row items-center'>
-                                            <input type="text" placeholder='Enter document name...' className='aseinput pl-2 block w-full' />
+                                            <input type="text" placeholder='Enter document name...' className='aseinput pl-2 block w-full' name='title' value={doc.title} onChange={handleTitle}/>
                                         </div>
                                     </td>
                                 </tr>
@@ -25,7 +40,7 @@ function NewMeeting({showDialog, setShowDialog}) {
                                     <td style="width:20%"><h1 className='inline-block'>Author</h1></td>
                                     <td>
                                         <div className='flex flex-row items-center'>
-                                            <input type="text" placeholder='Enter author name...' className='aseinput pl-2 block w-full' />
+                                            <input type="text" placeholder='Enter author name...' name='writer' className='aseinput pl-2 block w-full' value={doc.writer} onChange={handleWriter} />
                                         </div>
                                     </td>
                                 </tr>
@@ -35,7 +50,7 @@ function NewMeeting({showDialog, setShowDialog}) {
                                     </td>
                                     <td>
                                         <div>
-                                            <textarea name="details" id="desc" rows="4" className='border border-asegrey w-full pl-2' placeholder='Add details...'></textarea>
+                                            <textarea name='description' id="desc" rows="4" className='border border-asegrey w-full pl-2' placeholder='Add details...' value={doc.description} onChange={handleDesc}></textarea>
                                         </div>
                                     </td>
                                 </tr>
@@ -44,7 +59,7 @@ function NewMeeting({showDialog, setShowDialog}) {
                     </div>
                 </PopUpContents>
                 <PopUpActions>
-                    <button id='add' className='ASE-button gap-2' onClick={()=>{setShowDialog(false);alert("Pura Puranya di save")}}>Save</button>
+                    <button id='add' className='ASE-button gap-2' onClick={()=>{setShowDialog(false);handleSave();}}>Save</button>
                     <button id='cancel' className='ASE-button bg-white text-black border-black border hover:bg-gray-300' onClick={()=>setShowDialog(false)}>Cancel</button>
                 </PopUpActions>
             </PopUpDialog>
@@ -52,7 +67,14 @@ function NewMeeting({showDialog, setShowDialog}) {
     )
 }
 
-function BorrowDoc({showDialogBor, setShowDialogBor}) {
+function BorrowDoc({showDialogBor, setShowDialogBor, onSubmit}) {
+    const [doc, setDoc] = useState({title: Date});
+    function handleSave() {
+        onSubmit(doc);
+    }
+    function handleReturn(e) {
+        setDoc({...doc, writer: e.target.value});
+    }
     return(
         <div className=''>
             <PopUpDialog open={showDialogBor} onChange={setShowDialogBor}>
@@ -78,15 +100,13 @@ function BorrowDoc({showDialogBor, setShowDialogBor}) {
                             </div>
                             <div className='flex items-center justify-center flex-row gap-5'>
                                     <h1 className='inline-block'>Return Date</h1>
-                                    <input type="date" name="tgl-event" id="tgl-event" className='block w-2/3 aseinput pl-2' />
+                                    <input type="date" name="return" id="return" className='block w-2/3 aseinput pl-2' value={doc.return} onChange={handleReturn} />
                             </div>
-                            
-                            
                         </form>
                     </div>
                 </PopUpContents>
                 <PopUpActions>
-                    <button id='add' className='ASE-button gap-2' onClick={()=>{setShowDialogBor(false);alert("Pura Puranya di save")}}>Save</button>
+                    <button id='add' className='ASE-button gap-2' onClick={()=>{setShowDialogBor(false);handleSave()}}>Save</button>
                     <button id='cancel' className='ASE-button bg-white text-black border-black border hover:bg-gray-300' onClick={()=>setShowDialogBor(false)}>Cancel</button>
                 </PopUpActions>
             </PopUpDialog>
@@ -104,7 +124,6 @@ export default function Document () {
 
     //Get Document
     async function getDocument(id) {
-        id = null;
         let response;
         const headers = {   
             'Authorization': 'Bearer ' + bearertoken
@@ -131,8 +150,30 @@ export default function Document () {
             'Authorization': 'Bearer ' + bearertoken
         }
         await fetch(apiurl + "/" + id, {method: 'DELETE', headers});
-        readDocData()
+        readDocData();
     }
+
+    //POST
+    async function submit(data) {
+        const headers = {
+            'Authorization': 'Bearer ' + bearertoken,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        let response;
+        if (data.id) {  
+            response = await fetch(apiurl + "/" + data.id, {method: 'PUT', headers, body: new URLSearchParams(data)});
+        } else {
+            response = await fetch(apiurl, {method: 'POST', headers, body: new URLSearchParams(data)});
+        }
+        if (response.status >= 400) {
+            const res = await response.json();
+            alert("Error", res.error);
+            return;
+        }
+        readDocData();
+        return response;
+    }
+
     useEffect(() => {
         readDocData();
     }, [])
@@ -190,7 +231,13 @@ export default function Document () {
                                         )}
                                     </div>
                                     </TableCol>
-                                    <TableCol>Rusdi</TableCol>
+                                    <TableCol>
+                                            {data.borrower ? (
+                                                data.borrower
+                                        ) : (
+                                            <p></p>
+                                        )}
+                                    </TableCol>
                                     
                                     <TableCol>
                                         <div className='flex flex-row items-center justify-center gap-3'>
@@ -215,10 +262,9 @@ export default function Document () {
                         </Table>
                     </div>
                 <button onClick={()=>setOpen(true)} className='self-end rounded-xl w-48 h-8 bg-white text-black border border-asegrey my-5 hover:bg-gray-200'>New Document</button>
-                <NewMeeting showDialog={showDialog} setShowDialog={setOpen}/>
-                <BorrowDoc showDialogBor={showDialogBor} setShowDialogBor={setOpenBor}/>
+                <NewDocument showDialog={showDialog} setShowDialog={setOpen} onSubmit={submit}/>
+                <BorrowDoc showDialogBor={showDialogBor} setShowDialogBor={setOpenBor} onSubmit={submit}/>
             </div>
         </div>
     )
 }
-
